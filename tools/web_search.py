99c6db095b65563003a974825a217e0616619dcc
luxwork4u-cpu <luxwork4u@gmail.com>
2026-04-09 18:23:33 -0400

    fix: update web_search tool to fix ddgs import error

--- tools/web_search.py
+++ tools/web_search.py
@@ -1,4 +1,4 @@
-﻿from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun
 
 web_search_tool = DuckDuckGoSearchRun(
     name="web_search",
